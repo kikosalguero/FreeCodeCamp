70e4b4b0f226f495882015d46adb38da944944ac
kikosalguero <hsfmello@gmail.com>
2016-05-22 14:37:15 +0200

    Remove quotes

--- teste.js
+++ teste.js
@@ -1,23 +1,24 @@
 // Setup
 var myObj = {
-  gift: "pony",
-  pet: "kitten",
-  bed: "sleigh"
+  "gift": "pony",
+  "pet": "kitten",
+  "bed": "sleigh"
 };
 
 function checkObj(checkProp) {
   // Your Code Here
   
-  if (myObj.hasOwnProperty("checkProp") === true) {
+  if (myObj.hasOwnProperty(checkProp) === true) {
     var answer = myObj.checkProp;
     return answer;
  
   }
   
-  else if (myObj.hasOwnProperty("checkProp") === false) { 
+  else if (myObj.hasOwnProperty(checkProp) === false) { 
   return "Not Found";
   }
 }
 
 // Test your code by modifying these values
 checkObj("gift");
+
